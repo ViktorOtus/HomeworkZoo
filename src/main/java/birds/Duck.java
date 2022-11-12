485c7d4a093c4865d874c1f098fa9c09ad08e1f5
ViktorOtus <114183106+ViktorOtus@users.noreply.github.com>
2022-11-12 19:38:50 +0300

    Update Duck.java

--- src/main/java/birds/Duck.java
+++ src/main/java/birds/Duck.java
@@ -6,7 +6,7 @@ import animals.Fly;
 public class Duck extends Animal implements Fly {
 
     public Duck(String name, String age, String weight, String color) {
-        super(name, Integer.parseInt(String.valueOf(age)), Integer.parseInt(String.valueOf(weight)), color);
+        super(name, Integer.parseInt(age), Integer.parseInt(weight), color);
     }
     public static void run() {
         System.out.println("Кря");
@@ -16,6 +16,5 @@ public class Duck extends Animal implements Fly {
     }
     @Override
     public void fly() {
-
     }
 }
