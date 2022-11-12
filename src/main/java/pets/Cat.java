e869f9baae5eb41121f5ffada53d4d7da3b585f6
ViktorOtus <114183106+ViktorOtus@users.noreply.github.com>
2022-11-12 19:37:43 +0300

    Update Cat.java

--- src/main/java/pets/Cat.java
+++ src/main/java/pets/Cat.java
@@ -4,7 +4,7 @@ import animals.Animal;
 
 public class Cat extends Animal {
     public Cat(String name, String age, String weight, String color) {
-        super(name, Integer.parseInt(age), Integer.parseInt(String.valueOf(weight)), color);
+        super(name, Integer.parseInt(age), Integer.parseInt(weight), color);
     }
     public static void run(){
         System.out.println("Мяу");
