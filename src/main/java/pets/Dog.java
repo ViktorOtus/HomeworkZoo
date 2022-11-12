126ab06d0d06d81aa54d89557fbb0e5cd204f845
ViktorOtus <114183106+ViktorOtus@users.noreply.github.com>
2022-11-12 19:38:21 +0300

    Update Dog.java

--- src/main/java/pets/Dog.java
+++ src/main/java/pets/Dog.java
@@ -4,11 +4,9 @@ import animals.Animal;
 
 public class Dog extends Animal {
     public Dog(String name, String age, String weight, String color) {
-        super(name, Integer.parseInt(String.valueOf(age)), Integer.parseInt(String.valueOf(weight)), color);
+        super(name, Integer.parseInt(age), Integer.parseInt(weight), color);
     }
     public static void run(){
         System.out.println("Гав");
-
     }
-
 }
